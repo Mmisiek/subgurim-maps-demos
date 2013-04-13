992dae3e57b494a5bffae5f4ee3156f6bfc13764
Marc Climent <mcliment@gmail.com>
2013-04-13 11:58:32 +0200

    Code clean-up

--- Others/UpdatePanels.aspx.designer.cs
+++ Others/UpdatePanels.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace GMaps.Demos.Others {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
@@ -56,14 +65,5 @@ namespace GMaps.Demos.Others {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.UpdatePanel Panel3;
-        
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
